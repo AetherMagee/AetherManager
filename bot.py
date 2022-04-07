2ba50d18115f0489ebc92accc741c8cde65d6d08
AetherMagee <aethermagee@gmail.com>
2022-04-07 08:06:32 -0400

    I cannot believe i made this typo...

--- bot.py
+++ bot.py
@@ -379,7 +379,7 @@ async def captcha(event):
         async with bot.conversation(event.chat, timeout=20) as conv:
             reply = await conv.get_response(code_notify)
             while not reply.text.isnumeric() or not event.user.id == reply.sender.id:
-                reply = await conv.response(code_notify)
+                reply = await conv.get_response(code_notify)
     except asyncio.exceptions.TimeoutError:
         try:
             await code_notify.delete()
