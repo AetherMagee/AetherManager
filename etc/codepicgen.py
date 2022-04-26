6648855f84dbff402ead0a2758d7bebd2a1ae99e
AetherMagee <aethermagee@gmail.com>
2022-04-26 10:55:11 +0300

    Development reset

--- etc/codepicgen.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image, ImageDraw, ImageFont
-
-def generate(text):
-    image = Image.open("etc/gray_bg.jpg")
-    editable = ImageDraw.Draw(image)
-    font = ImageFont.truetype("etc/ShipporiAntique-Regular.ttf", 64)
-    editable.text((70, 30), text=text, font=font)
-    image.save(f"temp/code_{text}.jpg", optimize=True, quality=40)
