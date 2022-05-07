73f0a3b21820b90b7a1cbca700cfcd8c02c1fa31
AetherMagee <aethermagee@gmail.com>
2022-05-07 23:04:53 +0300

    why...

--- config.py
+++ config.py
@@ -2,4 +2,3 @@ API_ID = None # Your API ID from my.telegram.org, INT
 API_HASH = "" # Your API hash from my.telegram.org, STR
 STR_SESSION = "" # Your session, can be retrieved by various methods, STR
 BOT_TOKEN = "" # Your bot token from BotFather, STR
-amogus = "a"
